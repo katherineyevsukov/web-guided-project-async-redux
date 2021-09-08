ee5c5fa169db82a6dd06e86556c51a8aa26c1cc0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-08 13:40:33 -0400

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,6 +1,6 @@
-const FETCH_START = "FETCH_START";
-const FETCH_SUCCESS = "FETCH_SUCCESS";
-const FETCH_FAIL = "FETCH_FAIL";
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
 
 
 export const fetchStart = ()=> {
