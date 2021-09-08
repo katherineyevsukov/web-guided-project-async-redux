4dca16ccf9bf7a18e2a76124dd96c7845f469f00
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-08 13:52:39 -0400

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,12 +1,21 @@
+import axios from 'axios';
+
 export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAIL = "FETCH_FAIL";
 
 
-
 export const getPerson = () => {
     return (dispatch)=> {
-        
+        dispatch({type: FETCH_START});
+    
+        axios.get("https://randomuser.me/api/")
+            .then(resp=> {
+                props.fetchSuccess(resp.data.results[0]);
+            })
+            .catch(err=>{
+                props.fetchFail(err);
+            });
     }
 }
 
